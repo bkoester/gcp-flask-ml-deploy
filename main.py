eba988a2b3d105a5bb3fbdc74b66cda28e35e27d
Ben <bkoester@users.noreply.github.com>
2023-05-18 17:51:49 -0400

    Update main.py to test CD again

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD'
+    return 'Hello World! CD (another change for testing)'
 
 @app.route('/echo/<name>')
 def echo(name):
